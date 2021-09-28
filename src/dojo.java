b91ab149a8e0c8b0161035f064c185a273e430e5
Larissa Martins <larissamartins.job@gmail.com>
2021-09-28 12:42:17 -0400

    método de retornar média feito.

--- src/dojo.java
+++ src/dojo.java
@@ -1,5 +1,13 @@
 public class dojo {
     public static void main(String[] args) {
         System.out.println("Frase aqui dentro");
+        double media = retornaMedia(5, 8);
+        System.out.printf("A média é: %.2f ", media);
     }
+
+    static double retornaMedia(int n1, int n2){
+        double media = (n1+n2)/2.0;
+        return media;
+    }
+
 }
