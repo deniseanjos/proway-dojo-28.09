d51a7a15b37c8264bf984b4ff7fafbf9faf8d7c6
Alessandragr <alessandra-ribeiro@live.com>
2021-09-28 14:08:36 -0300

    Adiciona método média

--- src/dojo.java
+++ src/dojo.java
@@ -7,6 +7,8 @@ public class dojo {
         String sobrenome = "Granemann";
         String nc = nomeCompleto(nome, sobrenome);
         System.out.println(nc); 
+        double respostaMedia = media(5.0, 30.1, 40.5, 55.6);
+        System.out.printf("A resposta da média é:%.2f ", respostaMedia);
     }
 
     static double retornaMedia(int n1, int n2){
@@ -18,5 +20,10 @@ public class dojo {
         String nome_completo = nome + " " + sobrenome; 
         return nome_completo;
     }
+    static double media(double num1, double num2, double num3, double num4) {
+        double soma = num1 + num2 + num3 + num4;
+        double resultado = soma/4;
+        return resultado;
+    } 
 
 }
